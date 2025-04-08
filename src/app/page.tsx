06461e251c402176925480357fa182d003304e66
@syedhamailmuq <hamailsyed139@gmail.com>
2025-04-08 19:20:26 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-white">
 
       {/* Company Logo */}
       <div className="fixed top-0 right-5 z-[1000]">
